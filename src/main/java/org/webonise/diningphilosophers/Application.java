ebc1ad54f457f6a52bf4e2d8b20fde9445e30a29
nitishm-webonise <nitish.mowall@weboniselab.com>
2016-09-21 18:44:24 +0530

    Dining Philosophers Changes
    1> added constant variables
    2> removed anonymous objects
    3> removed start thread from constructor
    4> removed hard coded variables

--- src/main/java/org/webonise/diningphilosophers/Application.java
+++ src/main/java/org/webonise/diningphilosophers/Application.java
@@ -1,15 +1,19 @@
-package org.webonise.dining_philosophers;
+package org.webonise.diningphilosophers;
+
+import java.util.concurrent.TimeUnit;
 
 public class Application {
-   public static void main(String[] args) {
-      //size - no of forks/philosophers
-      int size = 5;
+   //SIZE - no of forks/philosophers
+   static final int SIZE = 5;
 
+   public static void main(String[] args) {
+
+      System.out.println("\n=====Dining Philosophers Problem=====\n");
       //array of forks
-      Fork[] forks = new Fork[size];
+      Fork[] forks = new Fork[SIZE];
 
       //creating object of Fork for each index of forks array
-      for (int i = 0; i < size; i++) {
+      for (int i = 0; i < SIZE; i++) {
          forks[i] = new Fork();
       }
 
@@ -18,12 +22,30 @@ public class Application {
       // at the beginning the last philosopher is trying to
       // grabbing left fork before right fork and all
       // other philosophers are trying to grab right fork first.
-      for (int i = 0; i < size; i++) {
-         if (i < (size - 1)) {
-            new Philosopher(forks[i], forks[i + 1], i);
+      Thread threads[] = new Thread[SIZE];
+      Philosopher philosopher[] = new Philosopher[SIZE];
+      for (int i = 0; i < SIZE; i++) {
+         if (i < (SIZE - 1)) {
+            philosopher[i] = new Philosopher(forks[i], forks[i + 1], i);
+            threads[i] = new Thread(philosopher[i]);
+            threads[i].start();
          } else {
-            new Philosopher(forks[0], forks[i], i);
+            philosopher[i] = new Philosopher(forks[0], forks[i], i);
+            threads[i] = new Thread(philosopher[i]);
+            threads[i].start();
          }
       }
+
+      try {
+         TimeUnit.MILLISECONDS.sleep(10000);
+         for (int i = 0; i < SIZE; i++) {
+            philosopher[i].stopPhilosopher();
+            threads[i].join();
+         }
+      } catch (InterruptedException interruptedException) {
+         interruptedException.printStackTrace();
+      }
+
+      System.out.println("Main Thread Exits");
    }
 }
